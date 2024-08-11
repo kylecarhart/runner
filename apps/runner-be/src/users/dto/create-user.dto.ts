ef091f80518aa6c72c919f1b84caa4fd04a0d95d
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-08-11 19:56:19 -0400

    fix user dto extends

--- apps/runner-be/src/users/dto/create-user.dto.ts
+++ apps/runner-be/src/users/dto/create-user.dto.ts
@@ -1,8 +1,8 @@
-import { ApiProperty, OmitType } from "@nestjs/swagger";
+import { ApiProperty } from "@nestjs/swagger";
 import { UserPassword } from "../decorators/user-password.decorator";
 import { User } from "../entities/user.entity";
 
-export class CreateUserDto extends OmitType(User, ["id"] as const) {
+export class CreateUserDto extends User {
   @UserPassword()
   @ApiProperty({ example: "Password1!" })
   confirmPassword: string;
