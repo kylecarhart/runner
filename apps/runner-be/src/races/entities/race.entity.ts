190120d46a5bb0b1b9977c6881bf950d24a2beb4
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-08-11 17:59:22 -0400

    add some race entity properties

--- apps/runner-be/src/races/entities/race.entity.ts
+++ apps/runner-be/src/races/entities/race.entity.ts
@@ -1,1 +1,16 @@
-export class Race {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Length } from "class-validator";
+import { Base } from "src/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Race extends Base {
+  @Column()
+  @ApiProperty({ example: "The Big One" })
+  @Length(1, 64)
+  name: string;
+
+  @Column()
+  @ApiProperty({ example: "2021-10-31T00:00:00Z" })
+  date: Date;
+}
