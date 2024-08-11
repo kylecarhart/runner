ff0193b322b83b66d4dd5e916a46028ae7d52f5d
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-08-11 19:49:27 -0400

    set up entity relation between races and events

--- apps/runner-be/src/events/entities/event.entity.ts
+++ apps/runner-be/src/events/entities/event.entity.ts
@@ -1,1 +1,24 @@
-export class Event {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Length } from "class-validator";
+import { Base } from "src/database/base.entity";
+import { Race } from "src/races/entities/race.entity";
+import { Column, Entity, OneToMany } from "typeorm";
+
+@Entity()
+export class Event extends Base {
+  @Column()
+  @ApiProperty({ example: "The Big One" })
+  @Length(1, 64)
+  name: string;
+
+  @Column()
+  @ApiProperty({ example: "2021-10-15T00:00:00Z" })
+  startDate: Date;
+
+  @Column({ nullable: true })
+  @ApiProperty({ example: "2021-10-16T00:00:00Z" })
+  endDate?: Date;
+
+  @OneToMany(() => Race, (race) => race.event)
+  races: Race[];
+}
--- apps/runner-be/src/races/entities/race.entity.ts
+++ apps/runner-be/src/races/entities/race.entity.ts
@@ -1,16 +1,24 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Length } from "class-validator";
 import { Base } from "src/database/base.entity";
-import { Column, Entity } from "typeorm";
+import { Event } from "src/events/entities/event.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity()
 export class Race extends Base {
   @Column()
-  @ApiProperty({ example: "The Big One" })
+  @ApiProperty({ example: "The Big 5K" })
   @Length(1, 64)
   name: string;
 
   @Column()
+  @ApiProperty({ example: "5K" })
+  type: string;
+
+  @Column()
   @ApiProperty({ example: "2021-10-31T00:00:00Z" })
   date: Date;
+
+  @ManyToOne(() => Event, (event) => event.races)
+  event: Event;
 }
