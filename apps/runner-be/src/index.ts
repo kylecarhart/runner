a427ec60ca044af55b7a3ecf06171ab98c93e269
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-09-17 19:40:00 -0400

    install router and cors

--- apps/runner-be/src/index.ts
+++ apps/runner-be/src/index.ts
@@ -1,8 +1,18 @@
+import cors from "@koa/cors";
+import Router from "@koa/router";
 import Koa from "koa";
-const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
+const app = new Koa();
+const router = new Router();
+
+app.use(cors());
+
+router.get("/", (ctx, next) => {
+  // ctx.router available
 });
 
-app.listen(3000);
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
