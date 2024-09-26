33774ba0c63d258b96d0a6e0e9fcd4c2b97c6e99
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-09-26 19:41:12 -0400

    base openapi path

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,6 @@
 import "zod-openapi/extend";
 
+export * from "./openapi";
 export { document } from "./openapi";
 export * from "./response";
 export * from "./user";
--- packages/api/src/openapi.ts
+++ packages/api/src/openapi.ts
@@ -1,18 +1,29 @@
-import { z } from "zod";
-import { createDocument } from "zod-openapi";
+import { createDocument, ZodOpenApiPathsObject } from "zod-openapi";
 import { withSuccessResponseSchema } from "./response";
-import { GetUserResponseSchema } from "./user";
+import { GetUserParamsSchema, GetUserResponseSchema } from "./user";
 
 export const document = createDocument({
   openapi: "3.1.0",
+  servers: [
+    {
+      url: "http://localhost:3000/api/{version}",
+      description: "Local server",
+      variables: {
+        version: { enum: ["v1"], default: "v1" },
+      },
+    },
+  ],
   info: {
     title: "Runner API",
     version: "1.0.0",
   },
+  tags: [{ name: "Users", description: "Manage users" }],
   paths: {
-    "/api/v1/users/{id}": {
+    "/users/{id}": {
       get: {
-        requestParams: { path: z.object({ id: z.string() }) },
+        summary: "Get a user by id",
+        tags: ["Users"],
+        requestParams: { path: GetUserParamsSchema },
         // requestBody: {
         //   content: {
         //     "application/json": { schema: GetUserResponseSchema },
@@ -34,3 +45,39 @@ export const document = createDocument({
     },
   },
 });
+
+export const GetUserOpenApiPath = {
+  "/users/{id}": {
+    get: {
+      summary: "Get a user by id",
+      tags: ["Users"],
+      requestParams: { path: GetUserParamsSchema },
+      // requestBody: {
+      //   content: {
+      //     "application/json": { schema: GetUserResponseSchema },
+      //   },
+      // },
+      responses: {
+        "200": {
+          description: "200 OK",
+          content: {
+            "application/json": {
+              schema: withSuccessResponseSchema(GetUserResponseSchema).openapi({
+                ref: "GetUserResponse",
+              }),
+            },
+          },
+        },
+        "201": {
+          description: "400 Bad Request",
+        },
+      },
+    },
+  },
+} satisfies ZodOpenApiPathsObject;
+
+// 1. Have each individual contract supply the path and the method
+// 2. Combine all the contracts, merging any that have the same path.
+//   2a. Throw an error if two contracts have the same path and method.
+// 3. Merge the contracts into a single document
+// 4. Export the document
