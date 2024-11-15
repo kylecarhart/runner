1703c3c592bc3fbd2b195433710dd87a9be188e3
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-11-15 14:17:21 -0500

    add prettier sorting for package json files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,13 @@
 const baseConfig = {
   plugins: ["prettier-plugin-organize-imports"],
+  overrides: [
+    {
+      files: ["package.json"],
+      options: {
+        plugins: ["prettier-plugin-packagejson"],
+      },
+    },
+  ],
 };
 
 export default baseConfig;
