c957c0ba15134a47a0d667cc8d9120c3de0a4481
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-11-21 09:03:19 -0500

    add astro prettier plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,20 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
 const baseConfig = {
   plugins: ["prettier-plugin-organize-imports"],
   overrides: [
     {
-      files: ["package.json"],
+      files: ["**/package.json"],
       options: {
         plugins: ["prettier-plugin-packagejson"],
+      },
+    },
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+        plugins: ["prettier-plugin-astro"],
       },
     },
   ],
