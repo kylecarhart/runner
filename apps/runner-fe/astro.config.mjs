3942c728b9e24610c5ec25ea77e31c287ef8a4bf
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-11-20 23:44:23 -0500

    install astro react

--- apps/runner-fe/astro.config.mjs
+++ apps/runner-fe/astro.config.mjs
@@ -2,8 +2,11 @@
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: cloudflare(),
+  integrations: [react()],
 });
