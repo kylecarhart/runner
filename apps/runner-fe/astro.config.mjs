34055e61819d859f04691714f747cc4160f1302d
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-11-27 11:06:09 -0500

    install tailwind

--- apps/runner-fe/astro.config.mjs
+++ apps/runner-fe/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -11,5 +12,5 @@ export default defineConfig({
       enabled: true,
     },
   }),
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
