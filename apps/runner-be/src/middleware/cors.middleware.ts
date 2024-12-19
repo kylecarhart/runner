7602ce30c7b407306257705fa57580c817751a50
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-12-19 08:55:21 -0500

    update csrf and cors middleware allowed localhost origins

--- apps/runner-be/src/middleware/cors.middleware.ts
+++ apps/runner-be/src/middleware/cors.middleware.ts
@@ -3,20 +3,28 @@ import { createMiddleware } from "hono/factory";
 import { HonoEnv } from "../index.js";
 import { env, isDevelopment } from "../utils/env.js";
 
+// Only allow localhost if we're in development the origin is localhost
+const allowLocalhost = (origin: string) =>
+  isDevelopment() && origin.startsWith("http://localhost:")
+    ? origin
+    : undefined;
+
 /**
  * Cross Origin Resource Sharing (CORS) middleware.
  * @returns Hono CORS middleware
  */
 export const corsMiddleware = () =>
   createMiddleware<HonoEnv>((c, next) => {
-    const origins = [env().ALLOWED_ORIGIN];
-
-    if (isDevelopment()) {
-      origins.push("http://localhost:4321"); // Add localhost if not in production
-    }
+    const allowedOrigins = { [env().ALLOWED_ORIGIN]: true };
 
     const corsMiddleware = cors({
-      origin: origins, // Does not send "Access-Control-Allow-Origin" if theres no match.
+      // Does not send "Access-Control-Allow-Origin" if theres no match.
+      origin: (origin) => {
+        if (allowedOrigins[origin] || allowLocalhost(origin)) {
+          return origin;
+        }
+        return null;
+      },
       maxAge: 3600, // 1 hour cache for browsers to not send preflight requests
       credentials: true, // Allow cookies to be sent with requests
     });
