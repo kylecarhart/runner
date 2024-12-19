745418b427db4deffa65bd11bc56154e4952b5b2
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-12-19 08:47:53 -0500

    fix fetch credentials error

--- apps/runner-fe/src/clients/v1Client.ts
+++ apps/runner-fe/src/clients/v1Client.ts
@@ -1,12 +1,17 @@
 import type { AppType } from "@runner/runner-be";
 import { hc } from "hono/client";
 
-// TODO: I want to be be able to pass any cookies along if SSR
-// https://github.com/withastro/roadmap/discussions/190#discussioncomment-8911086
-// https://www.npmjs.com/package/astro-global
+/** @see https://github.com/cloudflare/workers-sdk/issues/2514#issuecomment-2178070014 */
+const isCredentialsSupported = "credentials" in Request.prototype;
+
+/**
+ * TODO: I want to be be able to pass any cookies along if SSR
+ * @see https://github.com/withastro/roadmap/discussions/190#discussioncomment-8911086
+ * @see https://www.npmjs.com/package/astro-global
+ */
 export const v1Client = hc<AppType>(import.meta.env.PUBLIC_API_URL, {
   init: {
-    credentials: "include", // Include cookies in all requests
+    ...(isCredentialsSupported && { credentials: "include" }), // Include cookies in all requests client side requests
   },
 }).api.v1;
 
@@ -20,20 +25,3 @@ export const getProfile = v1Client.users.profile.$get;
 export const signup = v1Client.auth.signup.$post;
 export const login = v1Client.auth.login.$post;
 export const confirmEmail = v1Client.auth["confirm-email"].$post;
-
-// export function getUser(id: string) {
-//   return v1Client.users[":id"].$get({ param: { id } });
-// }
-
-// export function getUsers(
-//   query: Parameters<typeof v1Client.users.$get>[0]["query"],
-// ) {
-//   return v1Client.users.$get({ query });
-// }
-
-// export function updateUser(
-//   id: string,
-//   body: Parameters<(typeof v1Client.users)[":id"]["$patch"]>[0]["json"],
-// ) {
-//   return v1Client.users[":id"].$patch({ param: { id }, json: body });
-// }
