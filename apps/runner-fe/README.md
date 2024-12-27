463d4494ebf0ceb1c3692ff242467c61c6dc1e53
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-12-26 20:35:32 -0500

    fix astro introspect bug

--- apps/runner-fe/README.md
+++ apps/runner-fe/README.md
@@ -16,3 +16,9 @@ All commands are run from the root of the project, from a terminal:
 | `npm run preview`         | Preview your build locally, before deploying     |
 | `npm run astro ...`       | Run CLI commands like `astro add`, `astro check` |
 | `npm run astro -- --help` | Get help using the Astro CLI                     |
+
+## Astro Inspect
+
+The Inspect app provides information about any islands on the current page. This will show you the properties passed to each island, and the client directive that is being used to render them.
+
+If the island overlay doesn't fully wrap the content, wrap the island in a div (within the island). Not sure if its a bug or not.
--- apps/runner-fe/src/forms/CreateEventForm.tsx
+++ apps/runner-fe/src/forms/CreateEventForm.tsx
@@ -69,109 +69,18 @@ export default function CreateEventForm() {
   };
 
   return (
-    <Form {...form}>
-      <h2 className="text-lg font-medium">Event Details</h2>
-      <form className="mt-2 flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          {/* Name */}
-          <FormField
-            control={control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Description */}
-          <FormField
-            control={control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Write a short description of the event"
-                    className="resize-none"
-                    rows={3}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* When? */}
-          <FormField
-            control={form.control}
-            name="startDate"
-            render={({ field }) => (
-              <FormItem className="">
-                <FormLabel>Event start date</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant="outline"
-                        className={cn(
-                          "w-[240px] pl-3 text-left font-normal h-10 flex",
-                          !field.value && "text-muted-foreground",
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={new Date(field.value)}
-                      onSelect={(date) => field.onChange(date?.toISOString())}
-                      disabled={(date) => date <= new Date()}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          {/* Where? */}
-          {/* Address */}
-          <FormField
-            control={control}
-            name="address"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Address</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="grid grid-cols-3 gap-4">
-            {/* City */}
+    <div>
+      <Form {...form}>
+        <h2 className="text-lg font-medium">Event Details</h2>
+        <form className="mt-2 flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup>
+            {/* Name */}
             <FormField
               control={control}
-              name="city"
+              name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>City</FormLabel>
+                  <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input {...field} />
                   </FormControl>
@@ -179,70 +88,155 @@ export default function CreateEventForm() {
                 </FormItem>
               )}
             />
-
-            {/* State */}
+            {/* Description */}
+            <FormField
+              control={control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Write a short description of the event"
+                      className="resize-none"
+                      rows={3}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* When? */}
             <FormField
               control={form.control}
-              name="state"
+              name="startDate"
               render={({ field }) => (
-                <FormItem>
-                  <FormLabel>State</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a state" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {US_STATES.map((state) => (
-                        <SelectItem key={state.value} value={state.value}>
-                          {state.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
+                <FormItem className="">
+                  <FormLabel>Event start date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal h-10 flex",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={new Date(field.value)}
+                        onSelect={(date) => field.onChange(date?.toISOString())}
+                        disabled={(date) => date <= new Date()}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
             />
-
-            {/* Zip */}
+            {/* Where? */}
+            {/* Address */}
             <FormField
               control={control}
-              name="zip"
+              name="address"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Zip</FormLabel>
+                  <FormLabel>Address</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input placeholder="" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-          </div>
-        </FormGroup>
-
-        {errors.root?.serverError && (
-          <p className="mt-1 text-sm text-red-500">
-            {errors.root.serverError.message}
-          </p>
-        )}
-
-        <Separator />
-
-        <Button
-          type="submit"
-          disabled={isSubmitting}
-          className="flex items-center justify-center gap-2"
-        >
-          {isSubmitting ? "Creating event..." : "Next"}
-          {isSubmitting && <LoaderCircle className="h-4 w-4 animate-spin" />}
-        </Button>
-      </form>
-    </Form>
+            <div className="grid grid-cols-3 gap-4">
+              {/* City */}
+              <FormField
+                control={control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* State */}
+              <FormField
+                control={form.control}
+                name="state"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>State</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a state" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {US_STATES.map((state) => (
+                          <SelectItem key={state.value} value={state.value}>
+                            {state.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Zip */}
+              <FormField
+                control={control}
+                name="zip"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Zip</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </FormGroup>
+          {errors.root?.serverError && (
+            <p className="mt-1 text-sm text-red-500">
+              {errors.root.serverError.message}
+            </p>
+          )}
+          <Separator />
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+            className="flex items-center justify-center gap-2"
+          >
+            {isSubmitting ? "Creating event..." : "Next"}
+            {isSubmitting && <LoaderCircle className="h-4 w-4 animate-spin" />}
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
