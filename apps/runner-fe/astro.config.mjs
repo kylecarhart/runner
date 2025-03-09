fe502334f722b82898056ac1848f8efcd2747f58
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-03-09 18:42:24 -0400

    last ditch effort

--- apps/runner-fe/astro.config.mjs
+++ apps/runner-fe/astro.config.mjs
@@ -19,12 +19,9 @@ export default defineConfig({
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
       // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias:
-        import.meta.env.MODE === "production"
-          ? {
-              "react-dom/server": "react-dom/server.edge",
-            }
-          : {},
+      alias: {
+        "react-dom/server": "react-dom/server.edge",
+      },
     },
   },
 });
