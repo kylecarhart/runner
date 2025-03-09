881a13c096e19edcd1305586e90f901db4a96ddc
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-03-09 17:28:27 -0400

    remove wrangler from runner-fe

--- apps/runner-fe/astro.config.mjs
+++ apps/runner-fe/astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
-      configPath: "./wrangler.jsonc",
+      // configPath: "./wrangler.jsonc",
     },
   }),
   integrations: [
